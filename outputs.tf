6afbfde4491d1d85923be81cf736c65ace35a8c1
Abdul Ahad <143015957+Delta451@users.noreply.github.com>
2026-02-10 19:23:39 +0530

    Add outputs for public subnet IDs and internet gateway

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ output "vpc_cidr_block" {
   description = "CIDR block of the VPC."
   value       = aws_vpc.main.cidr_block
 }
+output "public_subnet_ids" {
+  value       = aws_subnet.public[*].id
+  description = "Public subnet IDs"
+}
+
+output "internet_gateway_id" {
+  value       = aws_internet_gateway.igw.id
+}
