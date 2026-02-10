cda2e16cd3132661b29d93cc549b01778b5ff35a
Abdul Ahad <143015957+Delta451@users.noreply.github.com>
2026-02-10 19:21:25 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
