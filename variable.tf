0579a7a2cc9f436db2fade913cd46785cc51ad6c
Abdul Ahad <143015957+Delta451@users.noreply.github.com>
2026-02-12 20:34:14 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -9,3 +9,8 @@ variable "availability_zones" {
   type        = list(string)
   default     = ["ap-south-1a", "ap-south-1b"]
 }
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
