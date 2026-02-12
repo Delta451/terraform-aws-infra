094a247df6a46f2e7256b7f415ae575d9fdf244a
Abdul Ahad <143015957+Delta451@users.noreply.github.com>
2026-02-12 20:32:06 +0530

    Update public_subnet.tf

--- public_subnet.tf
+++ public_subnet.tf
@@ -9,3 +9,16 @@ variable "availability_zones" {
   type        = list(string)
   default     = ["ap-south-1a", "ap-south-1b"]
 }
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "${var.project_name}-public-${count.index + 1}"
+    Type      = "public"
+    ManagedBy = "Terraform"
+  }
+}
