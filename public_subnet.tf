31a2279dbd8048c83ac060ecfc9e46feafd517ce
Abdul Ahad <143015957+Delta451@users.noreply.github.com>
2026-06-24 20:38:22 +0530

    refactor: remove duplicate variable definitions from public_subnet.tf
    - Remove duplicate variable definitions (now in variable.tf)
    - Update tags to use merge for consistency

--- public_subnet.tf
+++ public_subnet.tf
@@ -1,14 +1,3 @@
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["ap-south-1a", "ap-south-1b"]
-}
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
@@ -16,9 +5,11 @@ resource "aws_subnet" "public" {
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
-  tags = {
-    Name      = "${var.project_name}-public-${count.index + 1}"
-    Type      = "public"
-    ManagedBy = "Terraform"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-public-${count.index + 1}"
+      Type = "public"
+    }
+  )
 }
